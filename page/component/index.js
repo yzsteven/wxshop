21a3738f61c50765fba0655cf268b0c20880737e
2273420731@qq.com <2273420731@qq.com>
2018-04-04 01:40:09 +0800

    work

--- page/component/index.js
+++ page/component/index.js
@@ -1,26 +1,27 @@
-wx.request({
-  url: 'http://localhost:8080/rebook/rflight', //仅为示例，并非真实的接口地址
-  data: {
-    cid : 1
-  },
-  header: {
-    'content-type': 'application/json' // 默认值
-  },
-  success: function (res) {
-    console.log(res.data)
-  }
-})
-
 Page({
   data: {
-    imgUrls: [
-      '/image/b1.jpg',
-      '/image/b2.jpg',
-      '/image/b3.jpg'
-    ],
     indicatorDots: false,
     autoplay: false,
     interval: 3000,
     duration: 800,
+  },
+  onLoad : function(){
+    var that = this;
+    var data;
+    wx.request({
+      url: 'https://www.lanrensc.cn/ysg-system/shop/index', //仅为示例，并非真实的接口地址
+      data: {
+        cid: 1
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data);
+        console.log(res.data.retValue.banner);
+        that.setData({banner:res.data.retValue.banner});
+        console.log(data);
+      }
+    })
   }
 })
