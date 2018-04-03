63a6e1973284fe60f6ea2dcd9c0b75db796967e4
2273420731@qq.com <2273420731@qq.com>
2018-04-03 23:16:22 +0800

    first

---
+++ page/component/category/category.js
@@ -0,0 +1,87 @@
+Page({
+    data: {
+        category: [
+            {name:'果味',id:'guowei'},
+            {name:'蔬菜',id:'shucai'},
+            {name:'炒货',id:'chaohuo'},
+            {name:'点心',id:'dianxin'},
+            {name:'粗茶',id:'cucha'},
+            {name:'淡饭',id:'danfan'}
+        ],
+        detail:[
+          {
+            "id": "guowei",
+            "banner": "/image/c1.png",
+            "cate": "guowei",
+            "detail": [
+                {
+                "thumb":"/image/c2.png",
+                "name":"梨花带雨 3个"
+                },{
+                "thumb":"/image/c2.png",
+                "name":"梨花带雨 3个"
+                }, {
+                  "thumb": "/image/c2.png",
+                  "name": "梨花带雨 3个"
+                }, {
+                  "thumb": "/image/c2.png",
+                  "name": "梨花带雨 3个"
+                }
+            ]
+          }, {
+            "id": "shucai",
+            "banner": "/image/c1.png",
+            "cate": "shucai",
+            "detail": [
+              {
+                "thumb": "/image/c2.png",
+                "name": "梨花带雨 3个"
+              }, {
+                "thumb": "/image/c2.png",
+                "name": "梨花带雨 3个"
+              }, {
+                "thumb": "/image/c2.png",
+                "name": "梨花带雨 3个"
+              }, {
+                "thumb": "/image/c2.png",
+                "name": "梨花带雨 3个"
+              }
+            ]
+          }
+        ],
+        curIndex: 0,
+        isScroll: false,
+        toView: 'guowei'
+    },
+    onReady(){
+        var self = this;
+        wx.request({
+            url:'http://www.gdfengshuo.com/api/wx/cate-detail.txt',
+            success(res){
+                self.setData({
+                    detail : res.data
+                })
+            }
+        });
+        
+    },
+    switchTab(e){
+      const self = this;
+      this.setData({
+        isScroll: true
+      })
+      setTimeout(function(){
+        self.setData({
+          toView: e.target.dataset.id,
+          curIndex: e.target.dataset.index
+        })
+      },0)
+      setTimeout(function () {
+        self.setData({
+          isScroll: false
+        })
+      },1)
+        
+    }
+    
+})
---
+++ page/component/index.js
@@ -0,0 +1,26 @@
+wx.request({
+  url: 'http://localhost:8080/rebook/rflight', //仅为示例，并非真实的接口地址
+  data: {
+    cid : 1
+  },
+  header: {
+    'content-type': 'application/json' // 默认值
+  },
+  success: function (res) {
+    console.log(res.data)
+  }
+})
+
+Page({
+  data: {
+    imgUrls: [
+      '/image/b1.jpg',
+      '/image/b2.jpg',
+      '/image/b3.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 800,
+  }
+})
